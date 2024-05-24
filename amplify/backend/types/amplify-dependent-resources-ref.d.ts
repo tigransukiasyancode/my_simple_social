334593e39d91fb0c7ae3218e2335572c74fd8a88
tigran <tigransuqiasyan@gmail.com>
2024-05-24 01:29:18 -0700

    Lesson - Add amplify to the project

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "mysimplesocial": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
