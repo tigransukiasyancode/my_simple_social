15f93c4062a9a4efb2a7be58db8d14f25546da61
tigran <tigransuqiasyan@gmail.com>
2024-05-23 22:59:08 -0700

    Adding list of Columns

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,68 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello World Title'),
-        ),
-        body: Center(
-          child: Text('Hello World Content'),
-        ),
-      ),
+      home: HomeScreen(title: "Home"),
     ),
   );
+}
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({required this.title});
+  String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('First Item'),
+                IconButton(
+                  onPressed: () {
+                    print('First Item');
+                  },
+                  icon: Icon(Icons.comment_rounded),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Second Item'),
+                IconButton(
+                    onPressed: () {
+                      print('Second Item');
+                    },
+                    icon: Icon(Icons.comment_rounded))
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Third Item'),
+                IconButton(
+                  onPressed: () {
+                    print('Third Item');
+                  },
+                  icon: Icon(Icons.comment_rounded),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
