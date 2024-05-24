08317b62d1d2be8be510f73fdcc2f7093c8eeafd
tigran <tigransuqiasyan@gmail.com>
2024-05-23 19:14:26 -0700

    Creating hello world

---
+++ android/app/src/main/kotlin/tigran/s/my_simple_social/MainActivity.kt
@@ -0,0 +1,5 @@
+package tigran.s.my_simple_social
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/tigran/suqiasyan/my_simple_social/MainActivity.kt
@@ -0,0 +1,6 @@
+package tigran.suqiasyan.my_simple_social
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
