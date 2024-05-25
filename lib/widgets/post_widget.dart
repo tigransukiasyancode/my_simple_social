23b605e38410ffc4d920e23d4c686cd29de20ac8
tigran <tigransuqiasyan@gmail.com>
2024-05-24 23:04:08 -0700

    Lesson - add comments bottom sheet

--- lib/widgets/post_widget.dart
+++ lib/widgets/post_widget.dart
@@ -22,6 +22,22 @@ class _PostWidgetState extends State<PostWidget> {
           Text(widget.text, style: TextStyle(fontSize: 20)),
           IconButton(
             onPressed: () {
+              showModalBottomSheet(context: context, builder: (context) {
+                return Container(
+                  height: 200,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text("Comment 1"),
+                        Text("Comment 2"),
+                        Text("Comment 3"),
+                      ],
+                    ),
+                  ),
+                );
+              });
               setState(() {
                 isCommentPressed = !isCommentPressed;
               });
