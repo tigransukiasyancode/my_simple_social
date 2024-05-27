3c87cf537653c75fb799b65c4706eea783972a9e
tigran <tigransuqiasyan@gmail.com>
2024-05-26 23:23:28 -0700

    Exercise - round corners to Post

--- lib/widgets/post_widget.dart
+++ lib/widgets/post_widget.dart
@@ -18,23 +18,30 @@ class PostWidget extends StatefulWidget {
 class _PostWidgetState extends State<PostWidget> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(widget.post.content, style: TextStyle(fontSize: 20)),
-          IconButton(
-            onPressed: () {
-              showModalBottomSheet(
-                  context: context,
-                  builder: (context) {
-                    return CommentsWidget(commentPostId:  widget.post.id);
-                  });
-            },
-            icon: Icon(Icons.comment_rounded),
-          ),
-        ],
+    return Container(
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(widget.post.content, style: TextStyle(fontSize: 20)),
+            IconButton(
+              onPressed: () {
+                showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return CommentsWidget(commentPostId:  widget.post.id);
+                    });
+              },
+              icon: Icon(Icons.comment_rounded),
+            ),
+          ],
+        ),
       ),
     );
   }
